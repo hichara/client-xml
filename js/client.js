149aa01a805601ffd8c55bc19fd14df127a6958a
adjibi004@gmail.com <adjibi004@gmail.com>
2015-04-27 05:09:07 +0200

    client final

--- js/client.js
+++ js/client.js
@@ -54,15 +54,73 @@ $(document).ready(function() {
 
 					$(".div_one_cv").slideUp();
 					$(".div_add_cv").slideUp();
-
 					$(".div_all_cv").html(txt);
-
 					$(".div_all_cv").slideDown();
 			}
 		};
 		
 		xhr.open("GET", "http://localhost:8080/1-1.0-SNAPSHOT/rest/resume/", true);
 		xhr.send(null);		
+	}
+
+	function get_one_resume() {
+		var xhr = getXMLHttpRequest();
+
+		var id_c = $("#numCV").val();
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+					var m_xml = xhr.responseXML;
+
+					var resumes = m_xml.getElementsByTagName("resume");
+
+					var firstname = m_xml.getElementsByTagName("firstName")[0].innerHTML;
+					var lastname = m_xml.getElementsByTagName("lastName")[0].innerHTML;
+
+					$(".nom_prenom").html("<h2>" + firstname + " " + lastname +"</h2");
+
+					var  txt = "<h3>Formation :</h3>";
+
+					var formations = m_xml.getElementsByTagName("formations");
+
+					for(var i = 0; i < formations.length; i++) {
+						txt = txt + "<h4>";
+						txt = txt + formations[i].getElementsByTagName("titre")[0].innerHTML;
+						txt = txt + "</h4>";
+						txt = txt + "<p>";
+						txt = txt + formations[i].getElementsByTagName("description")[0].innerHTML;
+						txt = txt + "</p>";
+					}
+
+					txt = txt + "</div>";
+
+					$(".display_formation").html(txt);
+
+					txt = "<h3> Langues </h3>";
+
+					var langues = m_xml.getElementsByTagName("langues");
+
+					txt = txt + "<ol>";
+					for(var i = 0;i < langues.length; i++) {
+						txt = txt + "<li>";
+						txt = txt + langues[i].getElementsByTagName("name")[0].innerHTML;
+						txt = txt + " - ";
+						txt = txt + langues[i].getElementsByTagName("niveau")[0].innerHTML;
+						txt = txt + "</li>";
+					}
+					txt = txt + "</ol>";
+
+					$(".display_langues").html(txt);
+
+					$(".div_add_cv").slideUp();
+					$(".div_all_cv").slideUp();
+					$(".div_one_cv").slideDown();
+
+			}
+		};
+		
+		xhr.open("GET", "http://localhost:8080/1-1.0-SNAPSHOT/rest/resume/" + id_c, true);
+		xhr.send(null);				
 	}
 
 
@@ -115,13 +173,9 @@ $(document).ready(function() {
 		
 		var txt = my_requeteBody();
 
-		alert(txt);
-
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-				alert(xhr.responseText);
-				$(".message_failure").slideUp();
-				$(".message_success").html("Succés de la requete");
+				alert("Success");
 			}
 
 			if(xhr.status == 400) {
@@ -141,27 +195,20 @@ $(document).ready(function() {
 	});
 
 	$(".btn_one_cv").click(function() {
-
-		$(".div_one_cv").slideDown();
-		$(".div_add_cv").slideUp();
-		$(".div_all_cv").slideUp();
-
+		get_one_resume();
 	});
 
 	$(".btn_add_cv").click(function() {
-
-		$(".div_display_cv").slideUp();
+		$(".div_one_cv").slideUp();
 		$(".div_add_cv").slideDown();
 		$(".div_all_cv").slideUp();
-
 	});
 
 	$(".btn_add_formation").click(function() {
 
 		var txt = $(".field_add_formation").html();
-
 		txt = txt + "<div class=\"form_field\">";
-		txt = txt + "<input type=\"text\" placeholder=\"re\"/> &nbsp <input type=\"text\" size=\"30\" placeholder=\"description\" /> <br/>";
+		txt = txt + "<input type=\"text\" placeholder=\"titre\"/> &nbsp <input type=\"text\" size=\"50\" placeholder=\"description\" /> <br/>";
 		txt = txt + "</div>"
 
 		$(".field_add_formation").html(txt);
@@ -170,9 +217,8 @@ $(document).ready(function() {
 	$(".btn_add_langue").click(function() {
 
 		var txt = $(".field_add_langue").html();
-
 		txt = txt + "<div class=\"field_langue\">"
-		txt = txt + "<input type=\"text\"/> &nbsp <select><option>1</option><option>2</option><option>3</option></select><br/>";
+		txt = txt + "<input type=\"text\"/> &nbsp niveau <select><option>1</option><option>2</option><option>3</option></select><br/>";
 		txt = txt + "</div>";
 
 		$(".field_add_langue").html(txt);
